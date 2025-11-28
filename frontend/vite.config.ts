b96f24fd67176a5accf16737edc6c810791c2630
Lionel <ibm@Lionels-MacBook-Air.local>
2025-11-28 16:42:42 +0200

    resolved to serve the correct file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/fitness-ecommerce/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/fit-team-prototype/' : '/',
   server: {
     port: 5173,
   },
